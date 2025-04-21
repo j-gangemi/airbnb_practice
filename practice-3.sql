62c94dda2720a82e8dc3162ff86bd7db75e13ea0
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-04-21 15:57:05 +0000

    practice files 1-3

--- practice-3.sql
+++ practice-3.sql
@@ -1,5 +1,11 @@
 -- What are all the neighborhoods, alphabetically?
 
+select
+    neighborhood
+from listings
+group by 1
+order by 1;
+
 -- +------------------------+
 -- | Albany Park            |
 -- | Archer Heights         |
